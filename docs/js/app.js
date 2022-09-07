eeac81126072507460ad6ef329253ae6b113a343
Zac Prejean <107084674+Zac-Prejean@users.noreply.github.com>
2022-09-07 14:40:09 -0500

    Add files via upload

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,16 +1,15 @@
 
 
-var dark = document.getElementById("dark");
 var icon = document.getElementById("computer");
 
+
 //--DARK MODE
-dark.onclick = function(){
-    document.body.classList.toggle("dark-mode");
-    if(document.body.classList.contains("dark-mode")){
-        dark.src= "images/Sun.svg";
-        icon.src= "images/Computer-icon-night.svg";
-    } else {
-        dark.src= "images/Moon.svg";
-        icon.src= "images/Computer-icon-day.svg";
-    }
+
+if (window.matchMedia("(prefers-color-scheme: dark)")){
+    icon.src= "images/Computer-icon-night.svg";
+} else if (window.matchMedia("(prefers-color-scheme: light)")){
+    icon.src= "images/Computer-icon-day.svg";
+} else {
 }
+
+
