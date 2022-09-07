45cb5b9ca4beb86f472cdcfa4f6abcd34e83d3a9
Zac Prejean <107084674+Zac-Prejean@users.noreply.github.com>
2022-09-07 16:24:17 -0500

    Add files via upload

--- docs/js/app.js
+++ docs/js/app.js
@@ -5,11 +5,10 @@ var icon = document.getElementById("computer");
 
 //--DARK MODE
 
-if (window.matchMedia("(prefers-color-scheme: dark)")){
-    icon.src= "images/Computer-icon-night.svg";
-} else if (window.matchMedia("(prefers-color-scheme: light)")){
+if (window.matchMedia("(prefers-color-scheme: light)")){
     icon.src= "images/Computer-icon-day.svg";
-} else {
+} else if (window.matchMedia("(prefers-color-scheme: dark)")){
+    icon.src= "images/Computer-icon-night.svg";
 }
 
 
